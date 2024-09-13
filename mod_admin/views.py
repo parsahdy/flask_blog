3ec2a07e908da3401d94506037dd7752eb4c0a15
Parsa Heydari <pheydari33@yahoo.com>
2024-09-13 13:03:35 +0330

    Update LoginForm in views.py

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -1,9 +1,12 @@
 from flask import session, render_template, request, abort, flash
+from . import admin
 from mod_users.forms import LoginForm
 from mod_users.models import User
-from . import admin
+from .utils import admin_only_view
+
 
 @admin.route('/')
+@admin_only_view
 def index():
     return 'Hello from admin'
 
@@ -20,9 +23,13 @@ def login():
         if not user.check_password(form.password.data):
             flash("Incorrect Credentials.", category='error')
             return render_template('admin/login.html', form=form)
+        if not user.is_admin():
+            flash("Incorrect Credentials.", category='error')
+            return render_template('admin/login.html', form=form)
         session['email'] = user.email
         session['user_id'] = user.id
+        session['role'] = user.role
         return "Logged in Successfully."
-    if session.get('email') is not None:
+    if session.get('role') == 1:
         return "You are already logged in."
-    return render_template('admin/login.html', form=form)
+    return render_template('admin/login.html', form=form) 
