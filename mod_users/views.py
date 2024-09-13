3c8e6a4757412a5bd096295ff4aa2a1edeb73122
Parsa Heydari <pheydari33@yahoo.com>
2024-09-13 13:40:11 +0330

    handeling database errors

--- mod_users/views.py
+++ mod_users/views.py
@@ -1,4 +1,5 @@
 from flask import request, render_template, flash
+from sqlalchemy.exc import IntegrityError
 from app import db
 from . import users
 from .models import User
@@ -14,11 +15,20 @@ def register():
             error_msg = 'Password and Confirm_password does not match'
             form.password.errors.append(error_msg)
             form.confirm_password.errors.append(error_msg)
+            return render_template('users/register.html', form=form)
+        old_user = User.query.filter(User.email.ilike(form.email.data).first())
+        if old_user:
+            flash("Email has been taken", "error")
+            return render_template('users/register.html', form=form)
         new_user = User()
         new_user.username = form.username.data
         new_user.email = form.email.data
         new_user.set_password(form.password.data)
-        db.session.add(new_user)
-        db.session.commit()
-        flash('You created your account successfully', 'success')
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            flash('You created your account successfully', 'success')
+        except IntegrityError:
+            db.session.rollback()
+            flash("Email has been taken", "error")
     return render_template('users/register.html', form=form)
