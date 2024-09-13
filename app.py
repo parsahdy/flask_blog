dd393f8b0a43e75135075de1d513bcea54b7d8bb
Parsa Heydari <pheydari33@yahoo.com>
2024-09-13 13:01:28 +0330

    modified app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import secrets
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_hex(16)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://parsa_blog:parsa@localhost:3000/flask_blog'
@@ -11,12 +12,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
 from mod_admin import admin
 from mod_users import users
 
 app.register_blueprint(admin)
 app.register_blueprint(users)
 
+
 if __name__ == '__main__':
+    from views import home
+    app.route('/')(home)
     app.run()
